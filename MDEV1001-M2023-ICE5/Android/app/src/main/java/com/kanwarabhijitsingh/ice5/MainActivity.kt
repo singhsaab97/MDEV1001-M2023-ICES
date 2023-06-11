e4f5488c8bdf964a57c8c74e924503db97f4bcd3
singhsaab97 <kanwarabhijitsingh@gmail.com>
2023-06-10 22:57:34 -0400

    added basic android ICE5 project

---
+++ MDEV1001-M2023-ICE5/Android/app/src/main/java/com/kanwarabhijitsingh/ice5/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.kanwarabhijitsingh.ice5
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+
+}
